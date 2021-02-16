35e5a7762204b942f12ddd06ae51566a409b568c
gabopereyra <gabrielp1689qgmail.com>
2021-02-16 18:31:44 -0300

    New tasks added (minified img)

--- Proyecto6SitioMusica/gulpfile.js
+++ Proyecto6SitioMusica/gulpfile.js
@@ -1,5 +1,6 @@
 const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
 
 //funcion que compila SASS
 function css(){
@@ -16,10 +17,17 @@ function minificarCss(){
         .pipe(dest('./build/css'))
 }
 
+function imagenes(){
+    return src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(dest('./build/img'))
+}
+
 function watchArchivos(){
     watch('src/scss/**/*.scss', css) //Al utilizar el *, le estamos indicando que escuche a todos los archivos de extensión scss que se encuentren en la carpeta
 }
 
 exports.css = css;
 exports.minificarCss = minificarCss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
