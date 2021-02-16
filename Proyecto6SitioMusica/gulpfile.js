8c712e370d449fd33d510282c37c532d5ecc8481
gabopereyra <gabrielp1689qgmail.com>
2021-02-16 19:55:55 -0300

    New dependencie added: webp

--- Proyecto6SitioMusica/gulpfile.js
+++ Proyecto6SitioMusica/gulpfile.js
@@ -1,16 +1,24 @@
-const { series, src, dest, watch } = require('gulp');
+const { series, src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+
+//Paths
+const paths = {
+    imagenes: ('src/img/**/*'),
+    scss: ('src/scss/**/*.scss')
+}
 
 //funcion que compila SASS
 function css(){
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe(sass())
         .pipe(dest('./build/css'))
 }
 
 function minificarCss(){
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe(sass({
             outputStyle: "compressed"
         }))
@@ -18,16 +26,25 @@ function minificarCss(){
 }
 
 function imagenes(){
-    return src('src/img/**/*')
+    return src(paths.imagenes)
     .pipe(imagemin())
     .pipe(dest('./build/img'))
+    .pipe(notify({message: 'Imagen minificada'}));
+}
+
+function versionWebp(){
+    return src('src/img/**/*')
+    .pipe(webp())
+    .pipe(dest('./build/img'))
+    .pipe(notify({message: 'Imagen webp'}));
 }
 
 function watchArchivos(){
-    watch('src/scss/**/*.scss', css) //Al utilizar el *, le estamos indicando que escuche a todos los archivos de extensión scss que se encuentren en la carpeta
+    watch(paths.scss, css) //Al utilizar el *, le estamos indicando que escuche a todos los archivos de extensión scss que se encuentren en la carpeta
 }
 
 exports.css = css;
 exports.minificarCss = minificarCss;
 exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
 exports.watchArchivos = watchArchivos;
