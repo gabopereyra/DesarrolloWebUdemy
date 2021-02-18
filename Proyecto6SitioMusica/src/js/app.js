deb2d565e2fcc4faa5d38ac969350fec64533ec2
gabopereyra <gabrielp1689qgmail.com>
2021-02-18 20:04:11 -0300

    Error fixed, effect of navbar visible.

--- Proyecto6SitioMusica/src/js/app.js
+++ Proyecto6SitioMusica/src/js/app.js
@@ -1,12 +1,32 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
     scrollNav();
+
+    navegacionFija();
 });
 
-function scrollNav(){
+function navegacionFija() {
+
+    const barra = document.querySelector('.header');
+
+    // Registrar el Intersection Observer
+    const observer = new IntersectionObserver( function(entries) {
+        console.log(entries[0]);
+         if(entries[0].isIntersecting) {
+             barra.classList.remove('fijo');
+         } else {
+             barra.classList.add('fijo');
+         }
+    });
+
+    // Elemento a observar
+    observer.observe(document.querySelector('.imagen'));
+}
+
+function scrollNav() {
     const enlaces = document.querySelectorAll('.navegacion-principal a');
 
-    enlaces.forEach(function(enlace){
-        enlace.addEventListener('click', function(e){
+    enlaces.forEach( function( enlace ) {
+        enlace.addEventListener('click', function(e) {
             e.preventDefault();
             const seccion = document.querySelector(e.target.attributes.href.value);
 
